13b855b8e634340987cc244594d36cf4e1c11be0
Ali Alsayed <a_alsayed87@hotmail.com>
2019-06-16 07:50:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,9 @@ function Ferret(name, age, foodRequirements){
   this.age = age;
   this.foodRequirements = foodRequirements;
 }
+
+
+function Cat(name, breed){
+  this.name = name;
+  this.breed = breed;
+}
